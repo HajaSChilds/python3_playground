dbe8fb5e06aaa309ee6b6e5a1fd1d5fbbc864230
HajaSC <hajasc@protonmail.com>
2020-07-01 22:19:07 -0400

    Initial Animal Class to Garden Game

---
+++ animals.py
@@ -0,0 +1,31 @@
+class Animal:
+    def __init__(self):
+        self.bite = bite
+    
+    def bite():
+        print("The animal bites you")
+
+    def eat():
+        print("The animal eats your garden") 
+
+    def run():
+        print("The animal runs away")
+
+    def dies():
+        print("You've killed the animal")
+
+class Fox(Animal):
+    pass
+
+class Raccoon(Animal):
+    pass
+
+class Snake(Animal):
+    pass
+
+class Squirrel(Animal):
+    pass
+
+class Rabbit(Animal):
+    pass
+
